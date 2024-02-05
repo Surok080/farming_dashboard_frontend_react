cffae47d3c9c4606910d395c304e31e9fe913229
alexanderkrivov <sashakrvv@yandex.ru>
2024-02-05 14:17:59 +0300

    тестовый коммит

--- src/components/map/Layers.jsx
+++ src/components/map/Layers.jsx
@@ -73,7 +73,7 @@ const Layers = memo(({ layer, activeArea, setActiveArea }) => {
             url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
           />
         </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer checked name="WorldImagery">
+        <LayersControl.BaseLayer name="WorldImagery">
           <TileLayer
             attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
