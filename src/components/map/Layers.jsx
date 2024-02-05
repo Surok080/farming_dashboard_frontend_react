8574e97212f152d231ca4cdb680b38adefc5183e
alexanderkrivov <sashakrvv@yandex.ru>
2024-02-05 14:07:26 +0300

    добвлен новый слой карты

--- src/components/map/Layers.jsx
+++ src/components/map/Layers.jsx
@@ -73,7 +73,12 @@ const Layers = memo(({ layer, activeArea, setActiveArea }) => {
             url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
           />
         </LayersControl.BaseLayer>
-
+        <LayersControl.BaseLayer checked name="WorldImagery">
+          <TileLayer
+            attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          />
+        </LayersControl.BaseLayer>
         {layer &&
           layer.features.map((item, key) => {
             return (
