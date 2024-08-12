0d3258ad64b7094d202cbb21eb9caad56c58f8da
alexanderkrivov <sashakrvv@yandex.ru>
2024-08-12 16:04:32 +0300

    Фикс команды билда

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
     // depending on your application, base can also be "/"
     base: '',
     plugins: [react()],
+    build: {
+      outDir: 'build',
+    },
     server: {    
         // this ensures that the browser opens upon server start
         open: true,
