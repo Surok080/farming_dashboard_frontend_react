f2a2a77d00a1f1c1f6b2c40553a24450a8744963
alexanderkrivov <sashakrvv@yandex.ru>
2024-09-03 23:01:55 +0300

    Доработка плана ТехКарты 3

--- src/components/dashboardPages/Techcart/LeftBlockPlan.jsx
+++ src/components/dashboardPages/Techcart/LeftBlockPlan.jsx
@@ -124,7 +124,7 @@ const LeftBlockPlan = ({ crops, year, fact, data, setData }) => {
             sx={{ fontWeight: "bold", color: "#62A65D" }}
             variant="h5"
           >
-            {data.crop.total_area}
+            {data.culture.total_area}
           </Typography>
         </Box>
         <Box width={"50%"} gap={1} display={"flex"} flexDirection={"column"}>
