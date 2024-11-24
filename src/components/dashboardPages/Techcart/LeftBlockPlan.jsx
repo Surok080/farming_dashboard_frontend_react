e8e7e306df57ebacab069f26b4b6eebc1eaa3106
alexanderkrivov <sashakrvv@yandex.ru>
2024-11-24 21:51:42 +0300

    hot fix v.3

--- src/components/dashboardPages/Techcart/LeftBlockPlan.jsx
+++ src/components/dashboardPages/Techcart/LeftBlockPlan.jsx
@@ -18,7 +18,7 @@ const LeftBlockPlan = ({crops, year, fact, data, setData}) => {
 
     function getTechCultivationValue() {
         const foundItem = crops.find(item => item.culture === crop);
-        if (foundItem) {
+        if (foundItem?.tech_cultivation.length > 0) {
             return foundItem.tech_cultivation.length > 0 ? foundItem.tech_cultivation[0] : 0;
         }
         return null; // Если не найдено соответствие, возвращаем null
