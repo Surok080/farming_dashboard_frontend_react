49f76f7c14d84758c1a3928c2c934525b6859878
alexanderkrivov <sashakrvv@yandex.ru>
2024-11-24 23:02:40 +0300

    hot fix v.11

--- src/components/dashboardPages/Techcart/LeftBlockPlan.jsx
+++ src/components/dashboardPages/Techcart/LeftBlockPlan.jsx
@@ -75,7 +75,7 @@ const LeftBlockPlan = ({crops, year, fact, data, setData}) => {
 
     const checkDisabled = () => {
         if (crop && crops.length > 0) {
-            return crops.filter(item => item.culture === crop)[0]?.tech_cultivation?.length <= 0 ? true : false
+            return crops.filter(item => item.culture === crop)[0]?.tech_cultivation?.length <= 0
         } else {
             return true;
         }
@@ -91,7 +91,7 @@ const LeftBlockPlan = ({crops, year, fact, data, setData}) => {
                 <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    value={crop}
+               d     value={crop}
                     label="Выберите культуру"
                     onChange={handleChange}
                 >
