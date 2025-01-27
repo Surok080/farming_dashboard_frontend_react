e4418ba9e652044a6b5b999df4797bea04e2b822
alexanderkrivov <sashakrvv@yandex.ru>
2025-01-27 09:49:37 +0300

    Проверка ci/cd

--- src/api/setup.js
+++ src/api/setup.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpService = axios.create({
-	baseURL: process.env.NODE_ENV === "development" ? 'http://127.0.0.1:8000' : `https://xn--80acqgarss0k.online/`,
+	baseURL: process.env.NODE_ENV === "development" ? 'http://127.0.0.1:8000' : `https://agro-connect.ru/`,
 	withCredentials: true,
 	headers: {
 		accept: 'application/json',
